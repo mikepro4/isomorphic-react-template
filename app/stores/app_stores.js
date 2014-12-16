2cac1a53d7dcc1ec0396481160a429bc2748ec75
Shawn Lim <shawn@thecarousell.com>
2014-12-16 14:16:32 +0800

    Revert "Revert 1088065..e5058ca"
    This reverts commit f1c2964c1715adc6bf5c968a2a81655ff15130fc.

--- app/stores/app_stores.js
+++ app/stores/app_stores.js
@@ -1,15 +1,17 @@
 /* Single actions file - You may split it into more files next time */
 var Reflux = require('reflux');
-var AppActions = require('../actions/app_actions.js');
+var Actions = require('../actions/app_actions.js');
 
-var AppStore = Reflux.createStore({
-    listenables: AppActions,
-    onFirstAction: function(){
-        
+var Store = Reflux.createStore({
+    listenables: Actions,
+    onFirstAction: function(date){
+      console.log('triggered first action');
+      if(date) console.log('got date parameter value: '+ date);
+      this.trigger();
     },
     onSecondAction: function(){
       
     }
 });
 
-module.exports = AppStore;
+module.exports = Store;
